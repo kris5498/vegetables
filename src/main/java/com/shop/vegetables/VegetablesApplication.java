95760c69f418ebc8ad60f5653ea8b9e66b087a6c
chai5498 <chaitanya5498@gmail.com>
2025-06-29 00:16:59 +0530

    Initial commit

---
+++ src/main/java/com/shop/vegetables/VegetablesApplication.java
@@ -0,0 +1,13 @@
+package com.shop.vegetables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VegetablesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VegetablesApplication.class, args);
+	}
+
+}
