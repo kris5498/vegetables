06ff26ea3dd4a20995af65f6dfe9cbafa3001ebc
chai5498 <chaitanya5498@gmail.com>
2025-06-29 18:32:42 +0530

    Added readme file

--- src/main/java/com/shop/vegetables/VegetablesApplication.java
+++ src/main/java/com/shop/vegetables/VegetablesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VegetablesApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(VegetablesApplication.class, args);
 	}
 
